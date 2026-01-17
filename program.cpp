a8bb8616d064da0181f0f99b86f581b9b9c7ff5e
filip <bakfilip500@gmail.com>
2026-01-17 23:41:19 +0100

    dodano sprawdzanie nieujemnosci

--- program.cpp
+++ program.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
+    if (n < 0) {
+        cout << "Błąd: liczba musi być nieujemna!" << endl;
+        return 1;
+    }
     cout << n << endl;
     return 0;
 }
