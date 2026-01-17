2299de60871530059a57168c8e6d91f3da596804
filip <bakfilip500@gmail.com>
2026-01-17 23:36:44 +0100

    dodano silnie

--- program.cpp
+++ program.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 using namespace std;
 
+long long silnia(int n) {
+    if (n == 0 || n == 1) return 1;
+    long long wynik = 1;
+    for (int i = 2; i <= n; i++) {
+        wynik *= i;
+    }
+    return wynik;
+}
+
 int main() {
     int n;
     cin >> n;
-    cout << n << endl;
+    cout << silnia(n) << endl;
     return 0;
 }
